f7b26905ba8244207f68d5681a5ad51ac7229396
John Roper <johnroper100@gmail.com>
2016-07-10 14:48:22 -0400

    Tried to re-add the custom node nothing working yet

--- nodes/main.py
+++ nodes/main.py
@@ -1,8 +1,74 @@
 import bpy
 from bpy.types import NodeTree, Node, NodeSocket
+from . import custom
 
 class CrowdMasterTree(NodeTree):
     '''The CrowdMaster node tree'''
     bl_idname = 'CrowdTreeType'
     bl_label = 'CrowdMaster'
     bl_icon = 'MOD_REMESH'
+
+class CrowdMasterTreeNode:
+    @classmethod
+    def poll(cls, ntree):
+        return ntree.bl_idname == 'CrowdTreeType'
+
+import nodeitems_utils
+from nodeitems_utils import NodeCategory, NodeItem
+
+class MyNodeCategory(NodeCategory):
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.tree_type == 'CrowdTreeType'
+
+# all categories in a list
+node_categories = [
+    # identifier, label, items list
+    MyNodeCategory("SOMENODES", "Some Nodes", items=[
+        # our basic node
+        NodeItem("CustomNodeType"),
+        ]),
+    MyNodeCategory("OTHERNODES", "Other Nodes", items=[
+        # the node item can have additional settings,
+        # which are applied to new nodes
+        # NB: settings values are stored as string expressions,
+        # for this reason they should be converted to strings using repr()
+        NodeItem("CustomNodeType", label="Node A", settings={
+            "myStringProperty": repr("Lorem ipsum dolor sit amet"),
+            "myFloatProperty": repr(1.0),
+            }),
+        NodeItem("CustomNodeType", label="Node B", settings={
+            "myStringProperty": repr("consectetur adipisicing elit"),
+            "myFloatProperty": repr(2.0),
+            }),
+        ]),
+    ]
+
+class MyCustomSocket(NodeSocket):
+    # Description string
+    '''Custom node socket type'''
+    # Optional identifier string. If not explicitly defined, the python class name is used.
+    bl_idname = 'CustomSocketType'
+    # Label for nice name display
+    bl_label = 'Custom Node Socket'
+
+    # Enum items list
+    my_items = [
+        ("DOWN", "Down", "Where your feet are"),
+        ("UP", "Up", "Where your head should be"),
+        ("LEFT", "Left", "Not right"),
+        ("RIGHT", "Right", "Not left")
+    ]
+
+    myEnumProperty = bpy.props.EnumProperty(name="Direction", description="Just an example", items=my_items, default='UP')
+
+    # Optional function for drawing the socket input value
+    def draw(self, context, layout, node, text):
+        if self.is_output or self.is_linked:
+            layout.label(text)
+        else:
+            layout.prop(self, "myEnumProperty", text=text)
+
+    # Socket color
+    def draw_color(self, context, node):
+        return (1.0, 0.4, 0.216, 0.5)
