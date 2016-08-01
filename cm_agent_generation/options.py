3895a2ebead50cb0f7e8f933a2bf251754511ef6
John Roper <johnroper100@gmail.com>
2016-07-31 22:15:37 -0400

    added min and max to the rand rot min and max

--- cm_agent_generation/options.py
+++ cm_agent_generation/options.py
@@ -18,8 +18,8 @@ bpy.types.Scene.positionType = bpy.props.EnumProperty(
         description = "Which layout algorithim the simulator should use.",
         default = "random")
 
-bpy.types.Scene.maxRandRot = FloatProperty(name="Max Rotation", description="The maximum random rotation in the Z axis for each agent.", default = 360.0)
-bpy.types.Scene.minRandRot = FloatProperty(name="Min Rotation", description="The minimum random rotation in the Z axis for each agent.", default = 0.0)
+bpy.types.Scene.maxRandRot = FloatProperty(name="Max Rotation", description="The maximum random rotation in the Z axis for each agent.", default = 360.0, max=360.0)
+bpy.types.Scene.minRandRot = FloatProperty(name="Min Rotation", description="The minimum random rotation in the Z axis for each agent.", default = 0.0, min=-360.0)
 
 bpy.types.Scene.positionMode = bpy.props.EnumProperty(
         items = [('vector', 'Vector', 'The span location is specified as a list of vector coordinates.'), 
