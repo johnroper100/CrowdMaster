0137479a8dd5974edbb441a037cd67e94b7829b4
Peter-Noble <peter@noblesque.org.uk>
2016-08-24 22:36:21 +0200

    Should have tested just before commiting!

--- libs/__init__.py
+++ libs/__init__.py
@@ -1,4 +1,14 @@
-from . import ins_vector
-from . import ins_clustering
-from . import ins_octree
+try:
+    from . import ins_vector
+except Exception:
+    print("ERROR importing ins_vector.py")
 
+try:
+    from . import ins_clustering
+except Exception:
+    print("ERROR importing ins_clustering")
+
+try:
+    from . import ins_octree
+except Exception:
+    print("ERROR importing ins_octree")
