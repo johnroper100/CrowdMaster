e832f653f4ce00bd81e9052a69cb5d666c3363eb
John Roper <johnroper100@gmail.com>
2016-08-31 18:36:19 -0400

    Delete MakeConnection.py

--- old-bp-code/MakeConnection.py
+++
@@ -1,9 +0,0 @@
-import bpy, sqlite3
-
-def MakeConnection():
-	#initialize sqlite3 connection and cursor
-	dbConn = sqlite3.connect("dbactors")
-	dbCursor = dbConn.cursor()
-
-	#register the sqlite3 cursor in the Blender object for other scripts to use
-	bpy.dbCursor = dbCursor
