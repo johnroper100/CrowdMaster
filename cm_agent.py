8ca224287850c19fb4c36927aa1db6cb593d00fc
Peter Noble <peter@noblesque.org.uk>
2016-09-06 21:47:41 +0100

    Don't insert keyframes when value is constant

--- cm_agent.py
+++ cm_agent.py
@@ -36,28 +36,34 @@ class Agent:
         self.arx = objs[blenderid].rotation_euler[0]
         self.rx = 0
         self.rsx = 0
+        self.arxKey = True  # True if a keyframe was set last frame
 
         self.ary = objs[blenderid].rotation_euler[1]
         self.ry = 0
         self.rsy = 0
+        self.aryKey = True  # True if a keyframe was set last frame
 
         self.arz = objs[blenderid].rotation_euler[2]
         self.rz = 0
         self.rsz = 0
+        self.arzKey = True  # True if a keyframe was set last frame
 
         """ap - absolute pos, p - change pos by, s - speed"""
         self.apx = objs[blenderid].location[0]
         self.px = 0
         self.sx = 0
+        self.apxKey = True  # True if a keyframe was set last frame
 
         self.apy = objs[blenderid].location[1]
         self.py = 0
         self.sy = 0
+        self.apyKey = True  # True if a keyframe was set last frame
 
         self.apz = objs[blenderid].location[2]
         self.pz = 0
         self.sz = 0
-        
+        self.apzKey = True  # True if a keyframe was set last frame
+
         self.globalVelocity = mathutils.Vector([0,0,0])
 
         """Clear out the nla"""
@@ -120,24 +126,94 @@ class Agent:
 
     def apply(self):
         """Called in single thread after all agent.step() calls are done"""
-        objs = bpy.data.objects
+        obj = bpy.data.objects[self.id]
 
-        if objs[self.id].animation_data:
-            objs[self.id].animation_data.action_extrapolation = 'HOLD_FORWARD'
-            objs[self.id].animation_data.action_blend_type = 'ADD'
-
-        """Set objects rotation and location"""
-        objs[self.id].rotation_euler = (self.arx, self.ary, self.arz)
-        objs[self.id].location = (self.apx, self.apy, self.apz)
-        if objs[self.id].animation_data:
-            for track in objs[self.id].animation_data.nla_tracks:
+        if obj.animation_data:
+            obj.animation_data.action_extrapolation = 'HOLD_FORWARD'
+            obj.animation_data.action_blend_type = 'ADD'
+            for track in obj.animation_data.nla_tracks:
                 track.mute = False
 
-        """Set the keyframes"""
-        objs[self.id].keyframe_insert(data_path="rotation_euler",
-                                      frame=bpy.context.scene.frame_current)
-        objs[self.id].keyframe_insert(data_path="location",
-                                      frame=bpy.context.scene.frame_current)
+        """Set objects rotation and location"""
+
+        if abs(self.arx - obj.rotation_euler[0]) > 0.000001:
+            if not self.arxKey:
+                obj.keyframe_insert(data_path="rotation_euler",
+                                    index=0,
+                                    frame=bpy.context.scene.frame_current-1)
+                self.arxKey = True
+            obj.rotation_euler[0] = self.arx
+            obj.keyframe_insert(data_path="rotation_euler",
+                                index=0,
+                                frame=bpy.context.scene.frame_current)
+        else:
+            self.arxKey = False
+
+        if abs(self.ary - obj.rotation_euler[1]) > 0.000001:
+            if not self.aryKey:
+                obj.keyframe_insert(data_path="rotation_euler",
+                                    index=1,
+                                    frame=bpy.context.scene.frame_current-1)
+                self.aryKey = True
+            obj.rotation_euler[1] = self.ary
+            obj.keyframe_insert(data_path="rotation_euler",
+                                index=1,
+                                frame=bpy.context.scene.frame_current)
+        else:
+            self.aryKey = False
+
+        if abs(self.arz - obj.rotation_euler[2]) > 0.000001:
+            if not self.arzKey:
+                obj.keyframe_insert(data_path="rotation_euler",
+                                    index=2,
+                                    frame=bpy.context.scene.frame_current-1)
+                self.arzKey = True
+            obj.rotation_euler[2] = self.arz
+            obj.keyframe_insert(data_path="rotation_euler",
+                                index=2,
+                                frame=bpy.context.scene.frame_current)
+        else:
+            self.arzKey = False
+
+
+        if abs(self.apx - obj.location[0]) > 0.000001:
+            if not self.apxKey:
+                obj.keyframe_insert(data_path="location",
+                                    index=0,
+                                    frame=bpy.context.scene.frame_current-1)
+                self.apxKey = True
+            obj.location[0] = self.apx
+            obj.keyframe_insert(data_path="location",
+                                index=0,
+                                frame=bpy.context.scene.frame_current)
+        else:
+            self.apxKey = False
+
+        if abs(self.apy - obj.location[1]) > 0.000001:
+            if not self.apyKey:
+                obj.keyframe_insert(data_path="location",
+                                    index=1,
+                                    frame=bpy.context.scene.frame_current-1)
+                self.apyKey = True
+            obj.location[1] = self.apy
+            obj.keyframe_insert(data_path="location",
+                                index=1,
+                                frame=bpy.context.scene.frame_current)
+        else:
+            self.apyKey = False
+
+        if abs(self.apz - obj.location[2]) > 0.000001:
+            if not self.apzKey:
+                obj.keyframe_insert(data_path="location",
+                                    index=2,
+                                    frame=bpy.context.scene.frame_current-1)
+                self.apzKey = True
+            obj.location[2] = self.apz
+            obj.keyframe_insert(data_path="location",
+                                index=2,
+                                frame=bpy.context.scene.frame_current)
+        else:
+            self.apzKey = False
 
         self.access = copy.deepcopy(self.external)
 
