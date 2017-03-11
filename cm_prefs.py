7b4eab79204584d79bb0d456d54335b9e186c2f0
John Roper <johnroper100@gmail.com>
2017-03-11 15:11:12 -0500

    cleanup

--- cm_prefs.py
+++ cm_prefs.py
@@ -41,32 +41,32 @@ class CMPreferences(AddonPreferences):
     bl_idname = __package__
     scriptdir = bpy.path.abspath(os.path.dirname(__file__))
 
-    auto_check_update = bpy.props.BoolProperty(
+    auto_check_update = BoolProperty(
         name = "Auto-check for Update",
         description = "If enabled, auto-check for updates using an interval",
         default = False,
         )
 
-    updater_intrval_months = bpy.props.IntProperty(
+    updater_intrval_months = IntProperty(
         name='Months',
         description = "Number of months between checking for updates",
         default=0,
         min=0
         )
-    updater_intrval_days = bpy.props.IntProperty(
+    updater_intrval_days = IntProperty(
         name='Days',
         description = "Number of days between checking for updates",
         default=7,
         min=0,
         )
-    updater_intrval_hours = bpy.props.IntProperty(
+    updater_intrval_hours = IntProperty(
         name='Hours',
         description = "Number of hours between checking for updates",
         default=0,
         min=0,
         max=23
         )
-    updater_intrval_minutes = bpy.props.IntProperty(
+    updater_intrval_minutes = IntProperty(
         name='Minutes',
         description = "Number of minutes between checking for updates",
         default=0,
