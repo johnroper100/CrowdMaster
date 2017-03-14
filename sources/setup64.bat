6096316f06173650c2dbff2357659cc3f40253be
John Roper <johnroper100@gmail.com>
2017-03-14 08:40:56 -0400

    Cython works on windows!

---
+++ sources/setup64.bat
@@ -0,0 +1,5 @@
+C:\Users\johnr\python3\python.exe setup64.py build_ext --inplace
+
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\mt.exe" -inputresource:C:\Users\johnr\Documents\blender\2.78\python\bin\python35.dll;#2 -outputresource:C:\Users\johnr\Documents\CrowdMaster\cm_nodeFunctions.pyd;#2
+
+xcopy C:\Users\johnr\Documents\CrowdMaster\sources\cm_nodeFunctions.cp35-win_amd64.pyd C:\Users\johnr\Documents\CrowdMaster\cm_nodeFunctions.pyd
