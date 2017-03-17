b896ce42655e8b84aa29110a80054e2257ee0beb
John Roper <johnroper100@gmail.com>
2017-03-17 19:47:57 -0400

    pep8 for cm_prefs.py

--- cm_prefs.py
+++ cm_prefs.py
@@ -42,33 +42,33 @@ class CMPreferences(AddonPreferences):
     scriptdir = bpy.path.abspath(os.path.dirname(__file__))
 
     auto_check_update = BoolProperty(
-        name = "Auto-check for Update",
-        description = "If enabled, auto-check for updates using an interval",
-        default = False,
+        name="Auto-check for Update",
+        description="If enabled, auto-check for updates using an interval",
+        default=False,
         )
 
     updater_intrval_months = IntProperty(
         name='Months',
-        description = "Number of months between checking for updates",
+        description="Number of months between checking for updates",
         default=0,
         min=0
         )
     updater_intrval_days = IntProperty(
         name='Days',
-        description = "Number of days between checking for updates",
+        description="Number of days between checking for updates",
         default=7,
         min=0,
         )
     updater_intrval_hours = IntProperty(
         name='Hours',
-        description = "Number of hours between checking for updates",
+        description="Number of hours between checking for updates",
         default=0,
         min=0,
         max=23
         )
     updater_intrval_minutes = IntProperty(
         name='Minutes',
-        description = "Number of minutes between checking for updates",
+        description="Number of minutes between checking for updates",
         default=0,
         min=0,
         max=59
