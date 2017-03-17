d01b273d9c87cb9b621ea64b5564d1342dba71d9
John Roper <johnroper100@gmail.com>
2017-03-17 19:45:25 -0400

    pep8 for cm_iconLoad.py

--- cm_iconLoad.py
+++ cm_iconLoad.py
@@ -21,7 +21,9 @@ import os
 import bpy
 
 ICONS = 'node_tree_logo start_sim stop_sim ' \
-        'agents reset instant_setup add_agents plug code shuffle array target setup motion dice house email download'.split(' ')
+        'agents reset instant_setup add_agents ' \
+        'plug code shuffle array target setup ' \
+        'motion dice house email download'.split(' ')
 icon_collection = {}
 
 
