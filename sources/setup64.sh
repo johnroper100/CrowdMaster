ab95fe97a74d56315574def957c70c6ebb0d222d
Peter-Noble <peter@noblesque.org.uk>
2017-04-15 16:50:17 +0100

    Don't recompile every time

--- sources/setup64.sh
+++ sources/setup64.sh
@@ -3,4 +3,3 @@
 python3 ./setup64.py build_ext --inplace
 rm ../cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so
 cp ./cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so ../cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so
-rm ./cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so
