49839b005ca8d73c8ba9c6e7949952715644e39e
Peter-Noble <peter@noblesque.org.uk>
2017-04-15 16:02:34 +0100

    Compile on windows as well

--- sources/setup64.py
+++ sources/setup64.py
@@ -1,11 +1,17 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
-import Cython.Compiler.Options 
+import Cython.Compiler.Options
+import platform
 
 Cython.Compiler.Options.annotate = True
 
-ext_modules = [Extension("cm_nodeFunctions", ["cm_nodeFunctions.pyx"],extra_compile_args=['/Ox','/openmp','/GT','/arch:SSE2','/fp:fast'])]
+if platform.system() == "Linux":
+    extra_compile_args = ['-openmp']
+else:
+    extra_compile_args = ['/Ox','/openmp','/GT','/arch:SSE2','/fp:fast']
+
+ext_modules = [Extension("cm_nodeFunctions", ["cm_nodeFunctions.pyx"], extra_compile_args=extra_compile_args)]
 
 setup(
   name = 'CrowdMaster',
---
+++ sources/setup64.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 ./setup64.py build_ext --inplace
+rm ../cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so
+cp ./cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so ../cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so
+rm ./cm_nodeFunctions.cpython-35m-x86_64-linux-gnu.so
