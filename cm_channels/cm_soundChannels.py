8c7d41e1fbe492bfab62f8168a3843c6fa172431
Peter-Noble <peter@noblesque.org.uk>
2017-04-19 22:59:04 +0100

    Clean up

--- cm_channels/cm_soundChannels.py
+++ cm_channels/cm_soundChannels.py
@@ -121,20 +121,6 @@ class Channel:
             if dist <= val:
                 to = O[emitterid]
 
-                """target = to.location - ag.location
-
-                z = mathutils.Matrix.Rotation(ag.rotation_euler[2], 4, 'Z')
-                y = mathutils.Matrix.Rotation(ag.rotation_euler[1], 4, 'Y')
-                x = mathutils.Matrix.Rotation(ag.rotation_euler[0], 4, 'X')
-
-                rotation = x * y * z
-                relative = target * rotation
-
-                changez = math.atan2(relative[0], relative[1])/math.pi
-                changex = math.atan2(relative[2], relative[1])/math.pi"""
-                """m = cm_accelerate.makeRotationMatrix(ag.rotation_euler[0],
-                                                     ag.rotation_euler[1],
-                                                     ag.rotation_euler[2])"""
                 rotMat = self.sim.agents[self.userid].rotationMatrix
                 changez, changex = cm_accelerate.relativeRotation(to.location.x,
                                                                   to.location.y,
