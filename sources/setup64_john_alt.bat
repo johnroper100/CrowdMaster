c68d79711140f100494e1554455ac64b3c6baca6
John Roper <johnroper100@gmail.com>
2017-04-22 14:19:06 -0400

    updated the cython builder

---
+++ sources/setup64_john_alt.bat
@@ -0,0 +1,5 @@
+C:\Users\johnr\python3\python.exe setup64.py build_ext --inplace
+
+"C:\Program Files (x86)\Windows Kits\10\bin\x64\mt.exe" -inputresource:C:\Users\johnr\Documents\blender\2.78\python\bin\python35.dll;#2 -outputresource:C:\Users\johnr\Documents\CrowdMaster\cm_accelerate.pyd;#2
+
+xcopy C:\Users\johnr\Documents\CrowdMaster\sources\cm_accelerate.cp35-win_amd64.pyd C:\Users\johnr\Documents\CrowdMaster\libs\cm_accelerate.pyd
