41ea1c9418f4a10ea4f2110be9b9672706478747
Peter-Noble <peter@noblesque.org.uk>
2017-04-26 20:38:30 +0100

    linux build script delete correct file

--- sources/setup64.sh
+++ sources/setup64.sh
@@ -5,5 +5,5 @@ python3 ./setup64.py build_ext --inplace
 rm ../libs/cm_accelerate.cpython-35m-x86_64-linux-gnu.so
 cp ./cm_accelerate.cpython-35m-x86_64-linux-gnu.so ../libs/cm_accelerate.cpython-35m-x86_64-linux-gnu.so
 
-rm ../libs/cm_compileBrain.cpython-35m-x86_64-linux-gnu.so
+rm ../cm_compileBrain.cpython-35m-x86_64-linux-gnu.so
 cp ./cm_compileBrain.cpython-35m-x86_64-linux-gnu.so ../cm_compileBrain.cpython-35m-x86_64-linux-gnu.so
