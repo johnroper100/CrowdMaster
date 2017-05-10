24f3e6fa32ec6b1fba52575ea25c4151e672c423
Peter-Noble <peter@noblesque.org.uk>
2017-05-10 21:44:02 +0100

    Furth neuron colour test

--- libs/cm_testAccelerate.py
+++ libs/cm_testAccelerate.py
@@ -122,3 +122,37 @@ class AccelerateTestCase(unittest.TestCase):
         self.assertAlmostEqual(hue, h, 6)
         self.assertAlmostEqual(sat, s, 6)
         self.assertAlmostEqual(val, v, 6)
+
+    def testNeuronColour3(self):
+        output = {"None": 0}
+        if output:
+            val = 1
+            av = sum(output.values()) / len(output)
+            if av > 0:
+                startHue = 0.333
+            else:
+                startHue = 0.5
+
+            if av > 1:
+                hueChange = -(-(abs(av) + 1) / abs(av) + 2) * (1 / 3)
+                hue = 0.333 + hueChange
+                sat = 1
+            elif av < -1:
+                hueChange = (-(abs(av) + 1) / abs(av) + 2) * (1 / 3)
+                hue = 0.5 + hueChange
+                sat = 1
+            else:
+                hue = startHue
+
+            if abs(av) < 1:
+                sat = abs(av)**(1 / 2)
+            else:
+                sat = 1
+        else:
+            hue = 0
+            sat = 0
+            val = 0.5
+        h, s, v = cm_accelerate.neuronColour(sum(output.values()), len(output))
+        self.assertAlmostEqual(hue, h, 6)
+        self.assertAlmostEqual(sat, s, 6)
+        self.assertAlmostEqual(val, v, 6)
