15c69d85dc40b4c87bd9f82e3c186d2aa26beca3
Peter-Noble <peter@noblesque.org.uk>
2017-07-27 13:57:53 +0100

    Remove CFFI dependancy and build in separate modules

--- rust/include/libcm.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBCROWDMASTER_H_INCLUDED
-#define LIBCROWDMASTER_H_INCLUDED
-
-unsigned int sum(unsigned int a, unsigned int b);
-void onbytes(const char *bytes);
-void libcm_init(void);
-#endif
-
