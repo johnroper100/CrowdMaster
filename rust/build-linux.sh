ce9324147371e65b25ab5d9d795bd300289460c8
John Roper <johnroper100@gmail.com>
2017-09-06 16:27:13 -0400

    Verbose is far more than needed

--- rust/build-linux.sh
+++ rust/build-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cargo build --verbose --all
+cargo build --all
 mv ./target/debug/libchannel_sound.so ../cm_channels/channel_sound.so
 mv ./target/debug/libchannel_world.so ../cm_channels/channel_world.so
