8907e9b756eb534254eda532a1f226b0a9b68eb3
Peter-Noble <peter@noblesque.org.uk>
2017-09-09 01:15:46 +0100

    Optimise build

---
+++ rust/build-linux-release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cargo build --all --release
+mv ./target/release/libchannel_sound.so ../cm_channels/channel_sound.so
+mv ./target/release/libchannel_world.so ../cm_channels/channel_world.so
