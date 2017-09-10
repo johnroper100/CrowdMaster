3a7216704dbc2e08a20cf3186f5fe71e0c8c7b7d
John Roper <johnroper100@gmail.com>
2017-09-10 14:02:53 -0400

    add windows build scripts

---
+++ rust/build-win-debug.bat
@@ -0,0 +1,3 @@
+cargo build --all
+copy target\debug\channel_sound.dll ..\cm_channels\channel_sound.pyc
+copy target\debug\channel_world.dll ..\cm_channels\channel_world.pyc
---
+++ rust/build-win-release.bat
@@ -0,0 +1,3 @@
+cargo build --all --release
+copy target\release\channel_sound.dll ..\cm_channels\channel_sound.pyc
+copy target\release\channel_world.dll ..\cm_channels\channel_world.pyc
