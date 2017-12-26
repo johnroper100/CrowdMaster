a1052c42c270f17cb0bd470c348a569c9894f7fd
Unknown <peter@noblesque.org.uk>
2017-12-26 10:07:28 +0000

    Fix windows build scripts

--- rust/build-win-debug.bat
+++ rust/build-win-debug.bat
@@ -1,4 +1,4 @@
 cargo build --all
-copy target\debug\channel_sound.dll ..\cm_channels\channel_sound.pyc
-copy target\debug\channel_world.dll ..\cm_channels\channel_world.pyc
-copy target\debug\cm_gen.dll ..\cm_generation\cm_gen.pyc
+copy target\debug\channel_sound.dll ..\cm_channels\channel_sound.pyd
+copy target\debug\channel_world.dll ..\cm_channels\channel_world.pyd
+::copy target\debug\cm_gen.dll ..\cm_generation\cm_gen.pyd
