48906ab4423ff68162b2a2d1ef5f75cabc511df4
John Roper <johnroper100@gmail.com>
2017-12-29 17:29:00 -0500

    Update the min and max for simulation start and end frames

--- cm_blenderData.py
+++ cm_blenderData.py
@@ -41,11 +41,15 @@ def updateEndFrame(self, context):
 bpy.types.Scene.cm_sim_start_frame = IntProperty(
     name="Simulation Start Frame",
     default=1,
+    min=-1,
+    max=1048574,
     update=updateStartFrame,
 )
 bpy.types.Scene.cm_sim_end_frame = IntProperty(
     name="Simulation End Frame",
     default=250,
+    min=-1,
+    max=1048574,
     update=updateEndFrame,
 )
 
