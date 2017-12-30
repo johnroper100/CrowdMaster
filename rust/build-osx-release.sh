399e51890d1ee17d4392f9ae2b1ca69c4c97cb63
Unknown <peter@noblesque.org.uk>
2017-12-30 14:18:45 +0000

    Fix world channel not building on osx

--- rust/build-osx-release.sh
+++ rust/build-osx-release.sh
@@ -3,7 +3,7 @@
 cd ./channel_path
 cargo rustc --features "cpython/python3-sys" --release -- -C link-arg="-undefined" -C link-arg="dynamic_lookup"
 cd ../channel_world
--cargo rustc --features "cpython/python3-sys" --release -- -C link-arg="-undefined" -C link-arg="dynamic_lookup"
+cargo rustc --features "cpython/python3-sys" --release -- -C link-arg="-undefined" -C link-arg="dynamic_lookup"
 cd ..
 
 cp ./target/release/libchannel_path.dylib ../cm_channels/channel_path.so
